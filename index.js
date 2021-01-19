cbfe58bf4edc11220d2e690b9b0bb630ad87648e
Sungpil An <ansungpil1@gmail.com>
2021-01-19 15:52:56 -0500

    Added init function in index.js

--- index.js
+++ index.js
@@ -14,11 +14,51 @@ connection.connect((err) => {
     console.log("Connected!");
 
 });
-
+// Function to start
+function start() {
+    inquirer.prompt({
+        
+    })
+}
 function init() {
     inquirer.prompt([
         {
-            type:
+            type: "list",
+            choices: ["Engineering", "Finance", "Legal", "Marketing", "Sales", "Exit"],
+            message: "What department is the employee in?",
+            name: "department",
+        },
+        {
+            type: "input",
+            message: "What role is the employee?",
+            name: "role",
+        },
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "What is the employee's last name?",
+            name: "lastName",
+        },
+    ]).then(({ department, role, firstName, lastName }) => {
+        if (department === "Engineering") {
+            createEmployee(role, firstName, lastName);
+        } else if (choice === "Finance") {
+            createEmployee(role, firstName, lastName);
+        } else if (choice === "Legal") {
+            createEmployee(role, firstName, lastName);
+        } else if (choice === "Marketing") {
+            createEmployee(role, firstName, lastName);
+        } else if (choice === "Sales") {
+            createEmployee(role, firstName, lastName);
+        } else {
+            connection.end();
         }
-    ])
+    });
 }
+
+// function to create employee list added
+function createEmployee()
