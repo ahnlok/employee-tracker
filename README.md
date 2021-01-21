db357a629fa764b3baee245b79a8e49fe4c8b539
Sungpil An <ansungpil1@gmail.com>
2021-01-21 18:53:48 -0500

    Added readme file

---
+++ README.md
@@ -0,0 +1,44 @@
+# Employee Management Tracker
+
+## Description: 
+### This program allows the user to view, add, delete, and update: employee, department, and role.
+#### This program mainly uses MySQL with "inquirer" with JavaScript.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [URL](#url)
+* [Contact](#contact)
+* [Contributions](#contributions)
+* [License](#license)
+
+ 
+# Installation
+### Copy This:
+#### git clone: git@github.com:ahnlok/employee-tracker
+
+---
+
+# Usage
+### Here is the guidline gif and link for this project
+![usage](employee.gif)
+#### Link: <https://drive.google.com/file/d/101ND7_qdsueVlog18tt5cldQOaIoM1qL/view>
+---
+
+# URL
+#### GitHub Page: <https://github.com/ahnlok/employee-tracker>
+#### Deployed Github Page: <https://ahnlok.github.io/employee-tracker/>
+---
+
+# Contact
+### Email: <ansungpil1@gmail.com>
+### GitHub: <https://github.com/ahnlok>
+### LinkedIn: <http://linkedin.com/in/an-sungpil-95ab8490/>
+---
+
+# Contributions
+### If you want to be a contributor send me an email to <ansungpil1@gmail.com>
+---
+# License
+### License: MIT 
+### Copyright (c) [2021] [Sungpil An]
+---
--- employeeSchema.sql
+++ employeeSchema.sql
@@ -3,12 +3,14 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+-- Department Table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
+-- Role Table
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(50) NOT NULL,
@@ -17,6 +19,7 @@ CREATE TABLE role (
   PRIMARY KEY (id)
 );
 
+-- Employee Table
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR (50) NOT NULL,
@@ -25,27 +28,20 @@ CREATE TABLE employee (
   manager_id INT, 
   PRIMARY KEY (id)
 );
-
+-- Department seed
 INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Human Resources"), ("Legal"), ("Finance"), ("Artist");
 
+-- Role seed
 INSERT INTO role (title, salary, department_id)
 VALUES ("Marketing Manager", 100000, 7), ("Software Developer", 70000, 2), ("Lawyer", 60000, 3), ("Software Engineer", 90000, 2), ("Sales Rep", 60000, 5), ("Digital Artist", 70000, 6), ("Junior Data Analyst", 40000, 1);
 
+-- Employee seed
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Jack", "Sparrow", 9, 1), ("Steve", "Woziniak", 2, 1), ("Ron", "Swanson", 3, 1), ("Luke", "William", 4, 3), ("Julius", "Caesar", 5, 1), ("George", "Lucas", 6, 1), ("Anakin", "Skywalker", 1, 1);
--- Query for view all --
--- SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
 
--- -- Query for view all roles --
--- SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
+SELECT * FROM department;
 
--- --Query for getting employees --
--- SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee
+SELECT * FROM roles;
 
--- -- Query for updating --
--- UPDATE employee SET role_id = 3 WHERE id = 8;
--- UPDATE employee SET ? WHERE ?;
-
--- -- Query for Delete --
--- DELETE FROM department WHERE id = 13;
+SELECT * FROM employee;
